de53cd521862a27fd342b93653ad72b8873f1946
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-19 16:08:08 -0400

    commit tree 53fa84ebeb47c67d9682a0d2ea7e864db0156e12

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+// function writeCards(names, event) {
+//     let messages = [];
+//     for (let i = 0; i < names.length; i++) {
+//         messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+// };
+// return messages;
+// }
+
+// function countDown(positiveNumber) {
+//     while(positiveNumber >= 0) {
+//         console.log(positiveNumber);
+//         positiveNumber--;
+//     }
+
+// }
+
+function writeCards(names, event){
+    const messages = []
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    };
+    return messages
+}
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
